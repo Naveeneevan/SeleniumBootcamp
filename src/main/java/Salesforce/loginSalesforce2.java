ca61ae23fa9c7fc6db7441b9c8042ae914c6d27a
Naveeneevan <naveengovindu67@gmail.com>
2022-05-23 21:12:32 +0530

    First Commit

---
+++ src/main/java/Salesforce/loginSalesforce2.java
@@ -0,0 +1,32 @@
+package Salesforce;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginSalesforce2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		ChromeDriver driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://login.salesforce.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("mars@testleaf.com");
+		driver.findElement(By.id("password")).sendKeys("BootcampSel$123");
+		driver.findElement(By.id("Login")).click();
+		
+		
+		
+		
+
+	}
+
+}
